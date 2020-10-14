5805a645a763d6865151187b944a4e31dd5e9269
dev9414 <46513305+dev9414@users.noreply.github.com>
2020-10-14 12:19:08 -0700

    Create convertcamelcasespecialsymbol.cpp

---
+++ convertcamelcasespecialsymbol.cpp
@@ -0,0 +1,42 @@
+// CPP program to convert given sentence 
+/// to camel case. 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// Function to remove spaces and convert 
+// into camel case 
+string convert(string s) 
+{ 
+	int n = s.length(); 
+
+	int res_ind = 0; 
+
+	for (int i = 0; i < n; i++) { 
+				if(s[i]<48)
+                {
+                  continue;}
+		// check for spaces in the sentence 
+		if (s[i] == ' ') { 
+
+			// conversion into upper case 
+			s[i + 1] = toupper(s[i + 1]); 
+			continue; 
+		} 
+
+		// If not space, copy character 
+		else
+			s[res_ind++] = s[i];		 
+	} 
+
+	// return string to main 
+	return s.substr(0, res_ind); 
+} 
+
+// Driver program 
+int main() 
+{ 
+	string str;
+  cin>>str;
+	cout << convert(str); 
+	return 0; 
+} 
